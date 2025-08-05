753d58d2ce5a0eb9d600a525df548cf73df46a6b
Craig Wendorf <cwendorf@uwsp.edu>
2025-08-05 14:48:32 -0500

    Updated documentation

---
+++ R/spTools-package.R
@@ -0,0 +1,14 @@
+#' spTools: Additional Functions for the statpsych Package
+#'
+#' spTools is a small collection of functions that add to the array of tools in Doug Bonetts statpsych package. 
+#' Some functions provide calculations not present in statpsych, whereas other functions offer vectorized versions of statpsych functions. 
+#' Other functions attempt to further standardize statpsych output.
+#'
+#' @author
+#' **Maintainer**: Craig Wendorf 
+#' \email{cwendorf@uwsp.edu}
+#'
+#' @docType package
+#' @name spTools
+#' @aliases spTools-package
+"_PACKAGE"
